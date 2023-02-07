577d04fe2c46f3a57995eb32e168a73ad6f2728a
grummble-ai <73717764+grummble-ai@users.noreply.github.com>
2023-02-07 22:54:23 +0100

    Update welcomepage.py

--- widgets/welcomepage.py
+++ widgets/welcomepage.py
@@ -5,7 +5,7 @@ def create_welcomepage():
     st.write('''
     Welcome fellow Exile, glad to have you here! This is my 'website' where you can find all sorts of 
     useful tools for [Path of Exile](https://www.pathofexile.com/)! \n
-    Some of them are created by myself, others are just gathered here for your convenience. All of them are, to my 
+    Some of them are created by myself, others are gathered for your convenience. All of them are, to my 
     knowledge, free to use. \n ''')
 
     st.markdown("<img src=\"https://i.gifer.com/4j.gif\" width=\"200\">", unsafe_allow_html=True)
@@ -21,6 +21,6 @@ def create_welcomepage():
     You can also just navigate by using the menu on the top of the sidebar. Sounds great? Great. ''')
 
     st.write('''
-            Enjoy this project and please provide feedback on .. really anything: things you like, things you dislike,
-            and things you want to see in the future. You best contact me on my **[Discord](https://discord.com/channels/827836172184584214/981558870507929648)**.  \n
+            Enjoy this project and **please provide feedback** on .. really anything: things you like, things you dislike,
+            and things you want to see in the future. You best contact me on my **[Discord](https://discord.com/channels/827836172184584214/981558870507929648)** or **[GitHub](https://github.com/grummble-ai/PathOfExile-SkillGemMargins/issues)**.  \n
             ''')
