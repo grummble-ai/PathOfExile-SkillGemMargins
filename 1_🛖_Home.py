0434cfedbe0ac76895b577249a2d1f11e0e86186
grummble-ai <73717764+grummble-ai@users.noreply.github.com>
2023-02-13 22:00:12 +0100

    resolved dependencies

--- 1_🛖_Home.py
+++ 1_🛖_Home.py
@@ -1,16 +1,16 @@
 import widgets.welcomepage as welcomepage
 import widgets.initializer as initializer
 import streamlit as st
-from utility.firebase_operations import add_action_to_db
+# from utility.firebase_operations import add_action_to_db
 
 # initialize tracking
 if 'tracking_active' not in st.session_state:
     initializer.start_tracking()
 
-# add one to the counter in db whenever this site is refreshed
-add_action_to_db(st.session_state.db_connection,
-                 viewer_id=st.session_state.viewer_id,
-                 document=u"actions_home")
+# # add one to the counter in db whenever this site is refreshed
+# add_action_to_db(st.session_state.db_connection,
+#                  viewer_id=st.session_state.viewer_id,
+#                  document=u"actions_home")
 
 # create welcome page
 initializer.create_boilerplate(pagetitle="PoE Academy's Toolbox", version="", subheader="")
--- utility/firebase_operations.py
+++ utility/firebase_operations.py
@@ -1,86 +1,86 @@
-import datetime
-import firebase_admin
-from firebase_admin import firestore, credentials
-import json
-import streamlit as st
-
-
-def get_latest_id_from_document(db, document: str, field: str):
-    # get latest entry and assign this for the session, holy shit is this convoluted
-    views_ref = db.collection(f"{document}")
-    doc = views_ref.order_by(u"time", direction=firestore.Query.DESCENDING).limit(1).get()
-    my_dict = {el.id: el.to_dict() for el in doc}
-    last_entry = next(iter(my_dict.values()))
-    id = int(last_entry[field]) + 1
-    return id
-
-
-def update_field_of_document(db, document: str, field: str, id: int, val):
-    views_ref = db.collection(f"{document}")
-    views_ref.document(str(id)).set({
-        str(field): val,
-        "time": datetime.datetime.utcnow()
-    })
-
-
-def add_action_to_db(db, viewer_id:int, document:str):
-    # first add one to the counter
-    st.session_state.actions += 1
-    # update general field
-    update_field_of_document(db, document=u"actions", field=u"number", id=viewer_id, val=st.session_state.actions)
-
-    # then update page specific document
-    if document == "actions_skillgemleveling":
-        st.session_state.actions_skillgemleveling += 1
-        # then add to specific db field
-        update_field_of_document(db,
-                                 document=document,
-                                 field=u"number",
-                                 id=viewer_id,
-                                 val=st.session_state.actions_skillgemleveling)
-
-    elif document == "actions_home":
-        st.session_state.actions_home += 1
-        update_field_of_document(db,
-                                 document=document,
-                                 field=u"number",
-                                 id=viewer_id,
-                                 val=st.session_state.actions_home)
-    elif document == "actions_toolbox":
-        st.session_state.actions_toolbox += 1
-        update_field_of_document(db,
-                                 document=document,
-                                 field=u"number",
-                                 id=viewer_id,
-                                 val=st.session_state.actions_toolbox)
-    elif document == "actions_sextants":
-        st.session_state.actions_toolbox += 1
-        update_field_of_document(db,
-                                 document=document,
-                                 field=u"number",
-                                 id=viewer_id,
-                                 val=st.session_state.actions_toolbox)
-    else:
-        ValueError("Provide a suitable firestore document name.")
-
-
-def create_db_connection():
-    # create database connection
-    if not firebase_admin._apps:
-        key_dict = json.loads(st.secrets["textkey"])
-        cred = credentials.Certificate(key_dict)
-        app = firebase_admin.initialize_app(cred)
-    else:
-        app = firebase_admin.get_app()
-
-    db = firestore.client()
-
-    viewer_id = get_latest_id_from_document(db, u"views", u"number")
-
-    update_field_of_document(db, document=u"views", field=u"number", id=viewer_id, val=viewer_id)
-    update_field_of_document(db, document=u"actions", field=u"number", id=viewer_id, val="1")
-
-    return db, viewer_id
-
-
-
+# import datetime
+# import firebase_admin
+# from firebase_admin import firestore, credentials
+# import json
+# import streamlit as st
+#
+#
+# def get_latest_id_from_document(db, document: str, field: str):
+#     # get latest entry and assign this for the session, holy shit is this convoluted
+#     views_ref = db.collection(f"{document}")
+#     doc = views_ref.order_by(u"time", direction=firestore.Query.DESCENDING).limit(1).get()
+#     my_dict = {el.id: el.to_dict() for el in doc}
+#     last_entry = next(iter(my_dict.values()))
+#     id = int(last_entry[field]) + 1
+#     return id
+#
+#
+# def update_field_of_document(db, document: str, field: str, id: int, val):
+#     views_ref = db.collection(f"{document}")
+#     views_ref.document(str(id)).set({
+#         str(field): val,
+#         "time": datetime.datetime.utcnow()
+#     })
+#
+#
+# def add_action_to_db(db, viewer_id:int, document:str):
+#     # first add one to the counter
+#     st.session_state.actions += 1
+#     # update general field
+#     update_field_of_document(db, document=u"actions", field=u"number", id=viewer_id, val=st.session_state.actions)
+#
+#     # then update page specific document
+#     if document == "actions_skillgemleveling":
+#         st.session_state.actions_skillgemleveling += 1
+#         # then add to specific db field
+#         update_field_of_document(db,
+#                                  document=document,
+#                                  field=u"number",
+#                                  id=viewer_id,
+#                                  val=st.session_state.actions_skillgemleveling)
+#
+#     elif document == "actions_home":
+#         st.session_state.actions_home += 1
+#         update_field_of_document(db,
+#                                  document=document,
+#                                  field=u"number",
+#                                  id=viewer_id,
+#                                  val=st.session_state.actions_home)
+#     elif document == "actions_toolbox":
+#         st.session_state.actions_toolbox += 1
+#         update_field_of_document(db,
+#                                  document=document,
+#                                  field=u"number",
+#                                  id=viewer_id,
+#                                  val=st.session_state.actions_toolbox)
+#     elif document == "actions_sextants":
+#         st.session_state.actions_toolbox += 1
+#         update_field_of_document(db,
+#                                  document=document,
+#                                  field=u"number",
+#                                  id=viewer_id,
+#                                  val=st.session_state.actions_toolbox)
+#     else:
+#         ValueError("Provide a suitable firestore document name.")
+#
+#
+# def create_db_connection():
+#     # create database connection
+#     if not firebase_admin._apps:
+#         key_dict = json.loads(st.secrets["textkey"])
+#         cred = credentials.Certificate(key_dict)
+#         app = firebase_admin.initialize_app(cred)
+#     else:
+#         app = firebase_admin.get_app()
+#
+#     db = firestore.client()
+#
+#     viewer_id = get_latest_id_from_document(db, u"views", u"number")
+#
+#     update_field_of_document(db, document=u"views", field=u"number", id=viewer_id, val=viewer_id)
+#     update_field_of_document(db, document=u"actions", field=u"number", id=viewer_id, val="1")
+#
+#     return db, viewer_id
+#
+#
+#
