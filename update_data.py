aae022576ba0beb398118899dfd6c533a893a838
grummble-ai <73717764+grummble-ai@users.noreply.github.com>
2023-05-10 22:20:24 +0200

    data updated

--- update_data.py
+++ update_data.py
@@ -3,16 +3,16 @@ import utility.app1.skillgems_data_handler as data_handler
 import utility.app1.gem_margins as gem_margins
 
 # sextants
-from utility.app2.sextant_data_handler import load_TFTdata_from_github, mix_sextant_info_and_tft_data
-# from utility.app2.sextant_data_handler import load_TFTdata_from_github, mix_sextant_info_and_tft_data, exclude_sextants
+# from utility.app2.sextant_data_handler import load_TFTdata_from_github, mix_sextant_info_and_tft_data
+from utility.app2.sextant_data_handler import load_TFTdata_from_github, mix_sextant_info_and_tft_data, exclude_sextants
 
 ############################################################
 # Skill gems
 ############################################################
-# get data from poe.ninja and save to db
-data_handler.load_all_categories()
-# create the df from poe.ninja data
-gem_margins.create_json_data()
+# # get data from poe.ninja and save to db
+# data_handler.load_all_categories()
+# # create the df from poe.ninja data
+# gem_margins.create_json_data()
 
 
 ############################################################
@@ -20,4 +20,4 @@ gem_margins.create_json_data()
 ############################################################
 load_TFTdata_from_github()
 mix_sextant_info_and_tft_data()
-# exclude_sextants()
+exclude_sextants()
--- utility/app2/sextant_data_handler.py
+++ utility/app2/sextant_data_handler.py
@@ -2,28 +2,28 @@ import json
 import requests
 import pandas as pd
 from .. import data_handler as dh
-# from ortools.sat.python import cp_model as cp
+from ortools.sat.python import cp_model as cp
 
 SAVE_FILE_NAME = "sextant_info_and_data.json"
 URL_TFT_DATA = "https://raw.githubusercontent.com/The-Forbidden-Trove/tft-data-prices/master/lsc/bulk-compasses.json"
 
-#
-# class VarArraySolutionPrinter(cp.CpSolverSolutionCallback):
-#     """Print intermediate solutions."""
-#
-#     def __init__(self, variables):
-#         cp.CpSolverSolutionCallback.__init__(self)
-#         self.__variables = variables
-#         self.__solution_count = 0
-#
-#     def on_solution_callback(self):
-#         self.__solution_count += 1
-#         for v in self.__variables:
-#             print('%s=%i' % (v, self.Value(v)), end=' ')
-#         print()
-#
-#     def solution_count(self):
-#         return self.__solution_count
+
+class VarArraySolutionPrinter(cp.CpSolverSolutionCallback):
+    """Print intermediate solutions."""
+
+    def __init__(self, variables):
+        cp.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+        self.__solution_count = 0
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        for v in self.__variables:
+            print('%s=%i' % (v, self.Value(v)), end=' ')
+        print()
+
+    def solution_count(self):
+        return self.__solution_count
 
 
 def load_TFTdata_from_github():
