fb714f25d9d4ab3a5413e37c864fb2f40b9683a0
grummble-ai <73717764+grummble-ai@users.noreply.github.com>
2023-05-16 22:38:05 +0200

    .

---
+++ update_gem_data.py
@@ -0,0 +1,9 @@
+import utility.app1.skillgems_data_handler as data_handler
+import utility.app1.gem_margins as gem_margins
+
+
+# get data from poe.ninja and save to db
+data_handler.load_all_categories()
+# create the df from poe.ninja data
+gem_margins.create_json_data()
+
---
+++ update_sextant_data.py
@@ -0,0 +1,8 @@
+from utility.app2.sextant_data_handler import load_TFTdata_from_github, mix_sextant_info_and_tft_data, exclude_sextants
+
+############################################################
+# sextants
+############################################################
+load_TFTdata_from_github()
+mix_sextant_info_and_tft_data()
+exclude_sextants()
