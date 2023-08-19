c3a351a57407b1849bd85b474f57b9bd338d57e8
grummble-ai <73717764+grummble-ai@users.noreply.github.com>
2023-08-20 00:49:48 +0200

    data refresh

--- update_gem_data.py
+++ update_gem_data.py
@@ -5,6 +5,6 @@ import utility.app1.gem_margins as gem_margins
 # get data from poe.ninja and save to db
 data_handler.load_all_categories()
 
-# create the df from poe.ninja data 
+# create the df from poe.ninja data
 gem_margins.create_json_data()
 
